627f3ffdac48d2cf08ff50e92d4b06edc47d3898
Tashrik Anam <anampartho31@gmail.com>
2021-01-31 00:10:31 +0600

    Add getUser function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 // Form
 let form = document.querySelector("form");
 form.addEventListener("submit", getUser);
+
+// Get User Profile
+function getUser(e) {
+  e.preventDefault();
+
+  let formData = new FormData(e.target);
+  let userName = formData.get("username");
+
+  fetch(`https://api.github.com/users/${userName}`)
+    .then((res) => res.json())
+    .then((data) => showUser(data));
+}
